f7867f63fe76fc755c46aeb826e283243f1075c2
mohadeseh-mousavisaeid <mohadeseh.mousavisaeid@gmail.com>
2024-07-10 08:06:48 +0200

    refactor: lit_module for physics_based_models, WIP

--- training_and_testing/lit_modules/lit_module_physics_based.py
+++ training_and_testing/lit_modules/lit_module_physics_based.py
@@ -47,7 +47,17 @@ class LitModule(pl.LightningModule):
         """
         # TODO: You have to modify this based on your task, model and data. This is where most of the engineering happens!
         x, y = self.prep_data_for_step(batch)
-        y_hat= self(x)
+
+        y_hat_list = []
+        for k in range(self.future_sequence_length):
+            y_hat_k = self(x)
+            y_hat_list.append(y_hat_k)
+            # if y_hat_k.dim() < 3:
+            #     y_hat_k = y_hat_k.unsqueeze(1)
+            x = torch.cat([x[:, 1:, :], y_hat_k], dim=1)
+
+        y_hat = torch.stack(y_hat_list, dim=1).squeeze(dim=2)
+        
         loss = F.mse_loss(y_hat, y)
         self.log(f"{string}_loss", loss)
         return loss
